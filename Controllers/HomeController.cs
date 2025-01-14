9f821507a6fb7c3831c732b440c678f292b8786d
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-01-14 18:01:36 +0000

    feat: remove standard files from the project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using mini_project_csharp.Models;
 
 namespace mini_project_csharp.Controllers;
 
@@ -26,6 +25,6 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View();
     }
 }
