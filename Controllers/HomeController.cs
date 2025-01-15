15f431c32ef61356e84d0132a81f16b4dece19b2
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-01-15 20:34:19 +0000

    refactor: remove unnecessary (standard) code from HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,24 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
-namespace mini_project_csharp.Controllers;
-
-public class HomeController : Controller
+namespace mini_project_csharp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View();
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
     }
 }
