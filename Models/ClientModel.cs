cbe7e58581b679d754fb39695f5cf7a8cc8853ea
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-01-15 15:30:00 +0000

    feat: create a model for client entity

---
+++ Models/ClientModel.cs
@@ -0,0 +1,16 @@
+namespace mini_project_csharp.Models
+{
+    public class Client 
+    {
+        public int IdClient { get; set; }
+        public required string Nome { get; set; }
+        public string? Apelido { get; set; }
+        public string? Endereco { get; set; }
+        public string? Telefone { get; set; }
+        public required string Email { get; set; }
+        public string? Nif { get; set; }
+        public required string Password { get; set; }
+
+        public int IdCodPostal { get; set; }
+    }
+}
