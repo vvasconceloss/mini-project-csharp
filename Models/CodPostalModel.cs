4f13a3e19a19b5a101985a3f4c4f23fa90b03efa
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-01-15 15:30:43 +0000

    feat: create a model for postal code entity

---
+++ Models/CodPostalModel.cs
@@ -0,0 +1,7 @@
+namespace mini_project_csharp.Models {
+  public class CodPostal {
+    public int IdCodPostal { get; set; }
+    public string? Localidade { get; set; }
+    public string? CodPostalValue { get; set; }
+  }
+}
