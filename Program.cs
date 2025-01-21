8f3dbd68314cdd0c530be8603bb79ca15df0f1e8
dnovytska <darynanovytska2006@gmail.com>
2025-01-21 10:38:13 +0000

    feat: page delete(view and controller)

--- Program.cs
+++ Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Delete}/{action=Index}/{id?}");
 
 app.Run();
