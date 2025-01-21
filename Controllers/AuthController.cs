aad20835ea9d1c50e41adaa9cf87954523ab6483
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-01-21 15:24:49 +0000

    feat: create function to register users

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using mini_project_csharp.Data;
+using mini_project_csharp.Models;
 
 namespace mini_project_csharp.Controllers
 {
-  public class AuthController : Controller
+  public class AuthController(ApplicationDbContext context) : Controller
   {
-    public IActionResult Login()
+    private readonly ApplicationDbContext _context = context;
+
+        public IActionResult Login()
     {
         return View();
     }
@@ -12,6 +16,18 @@ namespace mini_project_csharp.Controllers
     public IActionResult Register()
     {
         return  View();
+    }
+
+    public IActionResult RegisterClient(Client client) {
+      if (ModelState.IsValid)
+      {
+        _context.Add(client);
+        _context.SaveChanges();
+
+        return RedirectToAction("Login", "Auth");
+      }
+
+      return View("Register", client);
     }
   }
 }
