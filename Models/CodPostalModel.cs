214999b037912b245f26d0907c5cb5a7ef4b6274
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-01-23 15:40:39 +0000

    feat: create a field for the formatted postcode

--- Models/CodPostalModel.cs
+++ Models/CodPostalModel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace mini_project_csharp.Models {
   public class CodPostal {
@@ -7,5 +8,18 @@ namespace mini_project_csharp.Models {
     public int IdCodPostal { get; set; }
     public string? Localidade { get; set; }
     public string? Codpostal { get; set; }
+
+    [NotMapped]
+    public string CodPostalFormatado
+    {
+      get
+      {
+        if (Codpostal?.Length == 7)
+        {
+          return Codpostal.Insert(4, "-") + " " + Localidade;
+        }
+        return Codpostal + " " + Localidade;
+      }
     }
+  }
 }
