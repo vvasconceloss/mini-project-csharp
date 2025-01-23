aab483d5b3017cd7a794dc15c959cfe7a1ed9f77
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-01-23 15:20:52 +0000

    feat: update the postcode model

--- Models/CodPostalModel.cs
+++ Models/CodPostalModel.cs
@@ -2,9 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace mini_project_csharp.Models {
   public class CodPostal {
+    
     [Key]
     public int IdCodPostal { get; set; }
     public string? Localidade { get; set; }
-    public string? CodPostalValue { get; set; }
-  }
+    public string? Codpostal { get; set; }
+    }
 }
